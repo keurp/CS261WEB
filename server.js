11ea06306ba30219b974a46b8c2b9080b23a2027
Pruek <pruek.rue@dome.tu.ac.th>
2024-10-18 00:47:57 +0700

    update server

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
 
